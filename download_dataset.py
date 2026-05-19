74b520e5393132fadfb98f0a1e8ea45fbe11267f
zacharydaniel229 <zacharydaniel229@gmail.com>
2026-05-18 22:56:45 -0500

    simplifies dataset download

--- download_dataset.py
+++ download_dataset.py
@@ -1,17 +1,7 @@
 import os
-import shutil
 import kagglehub
 
+# Download latest version
 path = kagglehub.dataset_download("alessiocorrado99/animals10")
-print(f"Downloaded to: {path}")
 
-# Move all contents to current directory
-for item in os.listdir(path):
-    src = os.path.join(path, item)
-    dst = os.path.join(os.getcwd(), item)
-    if os.path.isdir(src):
-        shutil.copytree(src, dst, dirs_exist_ok=True)
-    else:
-        shutil.copy2(src, dst)
-
-print("Copied to current directory")
+print("Path to dataset files:", path)
