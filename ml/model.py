b453f5cef04a746a169d01c667fc7df4c9c654e3
zacharydaniel229 <zacharydaniel229@gmail.com>
2026-05-21 17:18:04 -0500

    added model definition file

---
+++ ml/model.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AnimalCNN(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        # Convolution Layers
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=16,
+            kernel_size=3,
+            padding=1
+        )
+
+        self.conv2 = nn.Conv2d(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=3,
+            padding=1
+        )
+
+        self.conv3 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3,
+            padding=1
+        )
+
+        # Pooling Layer
+        self.pool = nn.MaxPool2d(
+            kernel_size=2,
+            stride=2
+        )
+
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(
+            64 * 16 * 16,
+            128
+        )
+
+        self.fc2 = nn.Linear(
+            128,
+            num_classes
+        )
+
+    def forward(self, x):
+
+        # Conv Block 1
+        x = self.pool(
+            F.relu(self.conv1(x))
+        )
+
+        # Conv Block 2
+        x = self.pool(
+            F.relu(self.conv2(x))
+        )
+
+        # Conv Block 3
+        x = self.pool(
+            F.relu(self.conv3(x))
+        )
+
+        # Flatten
+        x = torch.flatten(x, 1)
+
+        # Dense Layers
+        x = F.relu(self.fc1(x))
+
+        x = self.fc2(x)
+
+        return x
